583a09fd2a701dbde02a08f2dd08abd1dc133cc7
Novianawati <novianawati92@gmailcom>
2023-07-01 07:13:53 +0700

    frontend

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify:true,
+  basePath: process.env.NODE_ENV === 'production' ? '/my-sakai-app' : '',
+  publicRuntimeConfig: {
+    contextPath: process.env.NODE_ENV === 'production' ? '/my-sakai-app' : ''
+  }
 }
 
 module.exports = nextConfig
